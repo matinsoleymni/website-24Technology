892a4a46f6d2f0fac01c9ac88ce74cdb7fc37f0f
MatinSoleymani <matinsoleymni@gmail.com>
2023-06-21 21:11:20 +0330

    add files and run project

--- webserver.js
+++ webserver.js
@@ -1,39 +1,27 @@
 /* WEBSERVER  */
 if($.URL == domin|| $.URL == domin+'#' || $.URL == domin+'index.html'){
-    fetch("./assets/layouts/HomePage.html", { method: "POST" })
+    fetch("./assets/layouts/index.html", { method: "GET" })
     .then(res => res.text())
-    .then(txt => APP.innerHTML = txt);
+    .then(txt => ROOT.innerHTML = txt);
 
-}else if ($.URL == domin+'#Blog' || $.URl == domin+'index.html#Blog'){
-    fetch("./assets/layouts/Blog.html", { method: "POST" })
+}else if ($.URL == domin+'#home' || $.URl == domin+'index.html#home'){
+    fetch("/assets/layouts/work.html", { method: "GET" })
     .then(res => res.text())
-    .then(txt => APP.innerHTML = txt);
-}else if($.URL == domin+'#login' || $.URL == domin+'index.html#login'){
-    fetch('./assets/layouts/login.html')
-    .then(res => res.text())
-    .then(txt => APP.innerHTML= txt)
+    .then(txt => ROOT.innerHTML = txt);
 }else {
     APP.innerHTML = '403'
 }
 
 window.addEventListener('locationchange' , ()=> {
-    if($.URL == domin || $.URL == domin+'#' || $.URL == domin+'index.html'){
-        fetch("./assets/layouts/HomePage.html", { method: "POST" })
+    if($.URL == domin|| $.URL == domin+'#' || $.URL == domin+'index.html'){
+        fetch("./assets/layouts/index.html", { method: "GET" })
         .then(res => res.text())
-        .then(txt => APP.innerHTML = txt);
+        .then(txt => ROOT.innerHTML = txt);
     
-    }else if ($.URL == domin+'#Blog' || $.URl == domin+'index.html#Blog'){
-        fetch("./assets/layouts/Blog.html", { method: "POST" })
+    }else if ($.URL == domin+'#home' || $.URl == domin+'index.html#home'){
+        fetch("/assets/layouts/work.html", { method: "GET" })
         .then(res => res.text())
-        .then(txt => APP.innerHTML = txt);
-    }else if($.URL == domin+'#login' || $.URL == domin+'index.html#login'){
-        fetch('./assets/layouts/login.html')
-        .then(res => res.text())
-        .then(txt => APP.innerHTML= txt)
-    }else if($.URL == domin+'#Products' || $.URL == domin+'index.html$Products'){
-        fetch('./assets/layouts/Products.html')
-        .then(res => res.text())
-        .then(txt => APP.innerHTML= txt)
+        .then(txt => ROOT.innerHTML = txt);
     }else {
         APP.innerHTML = '403'
     }
